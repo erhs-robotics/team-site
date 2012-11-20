945e25afa4f251584b99cb9805aabe8080caf3f3
Nick Yaculak <nyaculak@gmail.com>
2012-11-20 18:32:01 -0500

    reorganizing main.py

--- main.py
+++ main.py
@@ -51,216 +51,6 @@ class OutreachHandler(Handler):
 		self.login()
 		self.render("outreach.html", user=self.user)
 		
-class StudentsHandler(Handler):
-    def get(self):        
-        self.login()
-        
-        members = db.GqlQuery("SELECT * FROM User")
-        members = list(members)    
-        members = sorted(members, key=lambda member: member.username.lower())
-        programmers = []
-        mechies = []
-        managers = []
-        outreachers = []
-        for member in members:
-            if member.team == "Programming":
-                programmers.append(member)
-            elif member.team == "Mechanical" or member.team == None:
-                mechies.append(member)
-            if member.team == "Management":
-                managers.append(member)
-            if member.team == "Outreach":
-                outreachers.append(member)
-        
-        self.render("students.html", user = self.user, users=members, display="none", programmers = programmers,
-					mechies = mechies, managers = managers, outreachers = outreachers)
-        
-    def post(self):
-        self.login()        
-        if self.user and CAN_MAKEUSER in self.user.privileges:
-            username = self.request.get('username')
-            password = self.request.get('password')
-            verify = self.request.get('verify')
-            email = self.request.get('email')            
-            fullname = self.request.get('fullname')          
-            
-            v_user = match(USER, username)
-            v_pass = match(PASS, password)
-            v_verify = None
-            if password == verify:
-                v_verify = 1       
-
-            v_email = 1
-            if email: v_email = match(EMAIL, email)
-
-            v_existing_user = db.GqlQuery('SELECT * FROM User WHERE username=:1', username)
-            v_existing_user = v_existing_user.count()
-
-            if v_user and v_pass and v_verify and v_email and v_existing_user < 1:
-                password = make_pw_hash(username, password)
-                newuser = User(username=username, password=password, email = email, isadmin=False, privileges=[IS_MEMBER])
-                if fullname: newuser.fullname = fullname
-                newuser.put()                
-                self.redirect('/about/students')
-            else:
-                m_user = ''
-                m_pass = ''
-                m_verify = ''
-                m_email = ''
-                if not v_user: m_user = 'not a valid username.'            
-                if not v_pass: m_pass = 'not a valid password.'
-                if not v_verify: m_verify = 'passwords do not match.'
-                if not v_email: m_email = 'not a valid email.'
-                if v_existing_user > 0: m_user = 'That user already exists.'
-                members = db.GqlQuery("SELECT * FROM User")
-                members = list(members)    
-                members = sorted(members, key=lambda member: member.username.lower())
-                programmers = []
-                mechies = []
-                managers = []
-                outreachers = []
-                for member in members:
-                    if member.team == "Programming":
-                        programmers.append(member)
-                    elif member.team == "Mechanical" or member.team == None:
-                        mechies.append(member)
-                    if member.team == "Management":
-                        managers.append(member)
-                    if member.team == "Outreach":
-                        outreachers.append(member)
-                self.render("students.html", user = self.user,
-                            email = m_email,
-                            username = m_user,
-                            password =  m_pass,
-                            verify = m_verify,
-                            mail = email,
-                            fullname = fullname,
-                            display = "block",
-                            users=members, programmers=programmers, mechies=mechies, managers=managers,
-                            outreachers=outreachers)
-
-class MentorsHandler(Handler):
-	def get(self):
-		self.login()
-		self.render("mentors.html", user=self.user)
-		
-class CompetitionsHandler(Handler):
-	def get(self):
-		self.login()
-		self.render("competitions.html", user=self.user)
-		
-class WebsiteHandler(Handler):
-	def get(self):
-		self.login()
-		self.render("website.html", user=self.user)
-		
-class FirstHandler(Handler):
-    def get(self):
-        self.login()
-        self.render("first.html", user = self.user)
-        
-class VexHandler(Handler):
-	def get(self):
-		self.login()
-		self.render("vex.html", user = self.user)
-		
-class MucHandler(Handler):
-	def get(self):
-		self.login()
-		self.render("muc.html", user = self.user)
-		
-class BlogHandler(Handler):
-    def get(self):
-        self.login()
-        
-        
-        #user = User(username="admin", password=make_pw_hash('admin', 'admin1234'), isadmin=True)
-        #user.put()
-        
-        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")      
-        self.render("blog.html", user = self.user, posts = list(posts))
-        
-class SponsorsHandler(Handler):
-    def get(self):
-        self.login()        
-        self.render("sponsors.html", user = self.user)
-        
-class GalleryHandler(Handler):
-    def get(self):
-        self.login()
-        self.render("gallery.html", user = self.user)
-
-class ResourcesHandler(Handler):
-    def get(self):
-        self.login()        
-        self.render("resources.html", user = self.user)
-        
-class ParentsHandler(Handler):
-	def get(self):
-		self.login()
-		self.render("parents.html", user = self.user)
-
-class DocsHandler(Handler):
-	def get(self):
-		self.login()
-		self.render("docs.html", user = self.user)
-
-class ContactHandler(Handler):
-    #def genRandomWord(self):
-    #    theword = ""
-    #    for i in range(6):
-    #        theword += random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
-    #    return theword
-    def get(self):
-        self.login()  
-        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")      
-        messages = db.GqlQuery("SELECT * FROM Message")# ORDER BY ID")
-        self.render("contact.html", user = self.user, messages = list(messages), posts = list(posts))
-    def post(self):
-        self.login()
-        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")      
-        messages = db.GqlQuery("SELECT * FROM Message")# ORDER BY ID")
-        sender_name = self.request.get('sender_name')
-        sender_email = self.request.get('sender_email')
-        sender_message = self.request.get('sender_message')
-        if sender_message != "":
-            newMessage = Message(name=sender_name, email=sender_email, message=sender_message)
-            newMessage.put()
-        self.render("contact.html", user=self.user, messages = list(messages), posts = list(posts))
-       
-### BACKENDS HANDLERS ###
-
-class LoginHandler(Handler):
-     def get(self):
-        self.login()
-                
-        self.render('login.html', user = self.user, remember = "false")
-
-     def post(self):        
-        username = self.request.get('username')
-        password = self.request.get('password')
-        remember = self.request.get('remember')       
-        
-
-        user = db.GqlQuery('SELECT * FROM User WHERE username=:1 LIMIT 1', username)        
-
-        if user.count() > 0 :
-            if valid_pw(username, password, user[0].password):
-                user_id = make_secure_val(str(user[0].key().id()))
-                header ="user_id=%s" % user_id
-                if remember == "true": header += "; expires=Wednesday, 01-Aug-2040 08:00:00 GMT"
-                self.response.headers.add_header('Set-Cookie', header)
-                self.redirect('/')
-            else:
-                self.render('login.html',username=username, error = "invalid password", remember=remember)
-        else:
-            self.render('login.html',username=username, error = "invalid login")
-
-class LogoutHandler(Handler):
-    def get(self):
-        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
-        self.redirect("/login")
-            
 class MembersHandler(Handler):
     def get(self):        
         self.login()
@@ -349,6 +139,128 @@ class MembersHandler(Handler):
                             display = "block",
                             users=members, programmers=programmers, mechies=mechies, managers=managers,
                             outreachers=outreachers)
+
+class MentorsHandler(Handler):
+	def get(self):
+		self.login()
+		self.render("mentors.html", user=self.user)
+		
+class CompetitionsHandler(Handler):
+	def get(self):
+		self.login()
+		self.render("competitions.html", user=self.user)
+		
+class WebsiteHandler(Handler):
+	def get(self):
+		self.login()
+		self.render("website.html", user=self.user)
+		
+class FirstHandler(Handler):
+    def get(self):
+        self.login()
+        self.render("first.html", user = self.user)
+        
+class FirstGameHandler(Handler):
+	def get(self):
+		self.login()
+		self.render("firstgame.html", user=self.user)
+        
+class VexHandler(Handler):
+	def get(self):
+		self.login()
+		self.render("vex.html", user = self.user)
+		
+class MucHandler(Handler):
+	def get(self):
+		self.login()
+		self.render("muc.html", user = self.user)
+		
+class BlogHandler(Handler):
+    def get(self):
+        self.login()
+        
+        
+        #user = User(username="admin", password=make_pw_hash('admin', 'admin1234'), isadmin=True)
+        #user.put()
+        
+        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")      
+        self.render("blog.html", user = self.user, posts = list(posts))
+        
+class SponsorsHandler(Handler):
+    def get(self):
+        self.login()        
+        self.render("sponsors.html", user = self.user)
+        
+class GalleryHandler(Handler):
+    def get(self):
+        self.login()
+        self.render("gallery.html", user = self.user)
+
+class ResourcesHandler(Handler):
+    def get(self):
+        self.login()        
+        self.render("resources.html", user = self.user)
+        
+class ParentsHandler(Handler):
+	def get(self):
+		self.login()
+		self.render("parents.html", user = self.user)
+
+class DocsHandler(Handler):
+	def get(self):
+		self.login()
+		self.render("docs.html", user = self.user)
+
+class ContactHandler(Handler):
+    def get(self):
+        self.login()  
+        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")      
+        messages = db.GqlQuery("SELECT * FROM Message")# ORDER BY ID")
+        self.render("contact.html", user = self.user, messages = list(messages), posts = list(posts))
+    def post(self):
+        self.login()
+        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")      
+        messages = db.GqlQuery("SELECT * FROM Message")# ORDER BY ID")
+        sender_name = self.request.get('sender_name')
+        sender_email = self.request.get('sender_email')
+        sender_message = self.request.get('sender_message')
+        if sender_message != "":
+            newMessage = Message(name=sender_name, email=sender_email, message=sender_message)
+            newMessage.put()
+        self.render("contact.html", user=self.user, messages = list(messages), posts = list(posts))
+       
+### BACKENDS HANDLERS ###
+
+class LoginHandler(Handler):
+     def get(self):
+        self.login()
+                
+        self.render('login.html', user = self.user, remember = "false")
+
+     def post(self):        
+        username = self.request.get('username')
+        password = self.request.get('password')
+        remember = self.request.get('remember')       
+        
+
+        user = db.GqlQuery('SELECT * FROM User WHERE username=:1 LIMIT 1', username)        
+
+        if user.count() > 0 :
+            if valid_pw(username, password, user[0].password):
+                user_id = make_secure_val(str(user[0].key().id()))
+                header ="user_id=%s" % user_id
+                if remember == "true": header += "; expires=Wednesday, 01-Aug-2040 08:00:00 GMT"
+                self.response.headers.add_header('Set-Cookie', header)
+                self.redirect('/')
+            else:
+                self.render('login.html',username=username, error = "invalid password", remember=remember)
+        else:
+            self.render('login.html',username=username, error = "invalid login")
+
+class LogoutHandler(Handler):
+    def get(self):
+        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+        self.redirect("/login")
 
 class NewpostHandler(Handler):
     def render_form(self, subject="", content="", error="",user=None):
@@ -627,8 +539,11 @@ app = webapp2.WSGIApplication([('/', MainHandler),
 									('/about/website', WebsiteHandler),
 							   ('/competitions', CompetitionsHandler),
 								   ('/first',FirstHandler),
+										#('/first/game', FirstGameHandler),
 								   ('/vex', VexHandler),
+										#('/vex/game', VexGameHandler),
 								   ('/muc', MucHandler),
+										#
                                ('/blog', BlogHandler),
                                ('/sponsors', SponsorsHandler),
                                ('/gallery', GalleryHandler),
