d0fc7f10b263f08e4e7b71a097e907a91fc58914
Nick Yaculak <nyaculak@gmail.com>
2013-07-28 20:03:22 +0000

    moving create user form to the control center

--- main.py
+++ main.py
@@ -111,22 +111,6 @@ class MembersHandler(Handler):
                 if not v_verify: m_verify = 'passwords do not match.'
                 if not v_email: m_email = 'not a valid email.'
                 if v_existing_user > 0: m_user = 'That user already exists.'
-                members = db.GqlQuery("SELECT * FROM User")
-                members = list(members)    
-                members = sorted(members, key=lambda member: member.username.lower())
-                programmers = []
-                mechies = []
-                managers = []
-                outreachers = []
-                for member in members:
-                    if member.team == "Programming":
-                        programmers.append(member)
-                    elif member.team == "Mechanical" or member.team == None:
-                        mechies.append(member)
-                    if member.team == "Management":
-                        managers.append(member)
-                    if member.team == "Outreach":
-                        outreachers.append(member)
                 self.render("members.html", user = self.user,
                             email = m_email,
                             username = m_user,
@@ -134,9 +118,7 @@ class MembersHandler(Handler):
                             verify = m_verify,
                             mail = email,
                             fullname = fullname,
-                            display = "block",
-                            users=members, programmers=programmers, mechies=mechies, managers=managers,
-                            outreachers=outreachers)
+                            display = "block")
         
 class BlogHandler(Handler):
     def getTotalPosts(self, post_list):
@@ -207,23 +189,71 @@ class ControlHandler(Handler):
 		self.render("control.html", user=self.user, slides=slides)
 	def post(self):
 		self.login()
-		if self.user and (CAN_POST in self.user.privileges or self.user.isadmin):
-			slide = Slide()
-			#image = self.request.get("image")
-			image = self.request.get('image')
-			caption = self.request.get("caption")
-			link = self.request.get("link")
-			if image and caption:
-				image = images.resize(self.request.get('image'), 440, 270)
-				slide.image = db.Blob(image)
-				slide.caption = caption
-				slide.link = link
-				slide.put()
-				self.redirect("/control")
-			else:
-				self.render("control.html", user=self.user, slides=Slide.all(), 
-				            image=image, caption=caption, link=link, 
-				            error="Please provide an image and a caption")
+		slideshow_submit = self.request.get("slideshow_submit")
+		createuser_submit = self.request.get("createuser_submit")
+		if slideshow_submit:
+			if self.user and (CAN_POST in self.user.privileges or self.user.isadmin):
+				slide = Slide()
+				#image = self.request.get("image")
+				image = self.request.get('image')
+				caption = self.request.get("caption")
+				link = self.request.get("link")
+				if image and caption:
+					image = images.resize(self.request.get('image'), 440, 270)
+					slide.image = db.Blob(image)
+					slide.caption = caption
+					slide.link = link
+					slide.put()
+					self.redirect("/control")
+				else:
+					self.render("control.html", user=self.user, slides=Slide.all(), 
+								image=image, caption=caption, link=link, 
+								error="Please provide an image and a caption")
+		if createuser_submit:
+			if self.user and (CAN_MAKEUSER in self.user.privileges or self.user.isadmin):
+				username = self.request.get('username')
+				password = self.request.get('password')
+				verify = self.request.get('verify')
+				email = self.request.get('email')            
+				fullname = self.request.get('fullname')          
+				
+				v_user = match(USER, username)
+				v_pass = match(PASS, password)
+				v_verify = None
+				if password == verify:
+					v_verify = 1       
+
+				v_email = 1
+				if email: v_email = match(EMAIL, email)
+
+				v_existing_user = db.GqlQuery('SELECT * FROM User WHERE username=:1', username)
+				v_existing_user = v_existing_user.count()
+
+				if v_user and v_pass and v_verify and v_email and v_existing_user < 1:
+					password = make_pw_hash(username, password)
+					newuser = User(username=username, password=password, email = email, isadmin=False, privileges=[IS_MEMBER])
+					if fullname: newuser.fullname = fullname
+					newuser.put()                
+					self.redirect('/control')
+				else:
+					m_user = ''
+					m_pass = ''
+					m_verify = ''
+					m_email = ''
+					if not v_user: m_user = 'not a valid username.'            
+					if not v_pass: m_pass = 'not a valid password.'
+					if not v_verify: m_verify = 'passwords do not match.'
+					if not v_email: m_email = 'not a valid email.'
+					if v_existing_user > 0: m_user = 'That user already exists.'
+					self.render("members.html", user = self.user,
+								email = m_email,
+								username = m_user,
+								password =  m_pass,
+								verify = m_verify,
+								mail = email,
+								fullname = fullname,
+								display = "block")
+			
        
 ### BACKENDS HANDLERS ###
 
