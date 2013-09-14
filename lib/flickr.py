2d4bbdfebcab58c20eb9739e01d791687ec57bac
Michael Stevens <mls0.5mv2@gmail.com>
2013-09-14 11:32:03 -0400

    removing print statements

--- lib/flickr.py
+++ lib/flickr.py
@@ -10,9 +10,7 @@ def getRequester(api_key):
 		kwargs["api_key"] = api_key
 		kwargs["format"] = "json"
 		kwargs["nojsoncallback"] = "1"		
-		for pair in kwargs.items(): url += "&%s=%s" % pair	
-		print "URL::: ", url
-		print "VALUE:::::", urllib2.urlopen(url).readlines()[0]	
+		for pair in kwargs.items(): url += "&%s=%s" % pair			
 		try:
 			info = json.loads(urllib2.urlopen(url).readlines()[0])
 			if info["stat"] == "ok": return info
