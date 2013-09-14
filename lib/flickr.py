ed8e3e1ec59ea086004a400045ae1bb109fb1435
Michael Stevens <mls0.5mv2@gmail.com>
2013-09-13 22:56:14 -0400

    added json parsing and imageUrl function

--- lib/flickr.py
+++ lib/flickr.py
@@ -1,4 +1,8 @@
 import urllib2
+import json
+
+API_KEY = ""
+GROUP_ID = "61141209@N05"
 
 def getRequester(api_key):
 	def makeRequest(**kwargs):
@@ -7,6 +11,15 @@ def getRequester(api_key):
 		kwargs["format"] = "json"
 		kwargs["nojsoncallback"] = "1"		
 		for pair in kwargs.items(): url += "&%s=%s" % pair		
-		return urllib2.urlopen(url).readlines()	
+		return json.loads(urllib2.urlopen(url).readlines())
 		
 	return makeRequest
+	
+def imageUrl(_info):#http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+	info = _info["photo"]
+	t = (info["farm"], info["server"], info["id"], info["secret"])
+	return "http://farm%s.staticflickr.com/%s/%s_%s.jpg" % t
+	
+
+
+	
